d6747407f77ff85b5adfa27c1c08b95fa148aa66
Silva.Yeghiazaryan <silva.yeghiazaryan@syngenta.com>
2025-12-18 15:40:51 +0400

    Added solution

--- task/2-task-n.py
+++ task/2-task-n.py
@@ -1,19 +1,16 @@
 from task.app.main import run
 
-# TODO:
-#  Try the `n` parameter with different models (`deployment_name`). With the parameter `n`, we can configure how many
-#       chat completion choices to generate for each input message
-#  User massage: Why is the snow white?
-
 # Models to try:
 # - gpt-4o
 # - claude-3-7-sonnet@20250219
 # - gemini-2.5-pro
 
 run(
-    # TODO:
-    #  1. Provide `deployment_name` with model from the list above👆
-    #  2. Use `n` parameter with value in range from 1 to 5!
+    # deployment_name='gemini-2.5-pro',
+    deployment_name='gpt-4o',
+    # n=1,
+    # n=3,
+    n=5
 )
 
 # Pay attention to the number of choices in the response!
